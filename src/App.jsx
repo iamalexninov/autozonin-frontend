3bbaa83bf9e0a3ceb8c16460048c8f67cbed8cca
Alex Georgiev Ninov <iamalexgninov@gmail.com>
2024-05-03 16:19:48 +0300

    added typography css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import './assets/styles/index.css'
+import "./assets/styles/index.css";
 
 function App() {
-  return <p>Welcome</p>;
+  return (
+    <>
+      <h1>Welcome</h1>
+      <h2>Welcome</h2>
+      <h3>Welcome</h3>
+      <h4>Welcome</h4>
+      <h5>Welcome</h5>
+      <p>dsdsads</p>
+      <li>dsadsas</li>
+      <a href="">link</a>
+      <button>submit</button>
+    </>
+  );
 }
 
 export default App;
