ea39f4ab203cbcee372d835aa11f8909412ff599
Alex Georgiev Ninov <iamalexgninov@gmail.com>
2024-05-04 15:45:30 +0300

    added custom select field

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,67 @@
+import { useState } from "react";
 import "./assets/styles/index.css";
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
 
 function App() {
+  const [checked, setChecked] = useState(false);
+  const [placeholderValue, setPlaceholderValue] = useState("Choose...");
+
+  const handleDropdownMenu = () => {
+    setChecked((prev) => !prev);
+  };
+
+  const handleInputClickedValue = (e) => {
+    setPlaceholderValue(e.target.value)
+  };
+
   return (
-    <>
-      <h1>Welcome</h1>
-      <h2>Welcome</h2>
-      <h3>Welcome</h3>
-      <h4>Welcome</h4>
-      <h5>Welcome</h5>
-      <p>dsdsads</p>
-      <li>dsadsas</li>
-      <a href="">link</a>
-      <button>submit</button>
-    </>
+    <div className="select">
+      <input type="radio" name="option" />
+      {checked ? (
+        <div className="toggle icon icon_up" onClick={handleDropdownMenu}>
+          <MdKeyboardArrowUp size={30} />
+        </div>
+      ) : (
+        <div className="toggle icon icon_down" onClick={handleDropdownMenu}>
+          <MdKeyboardArrowDown size={30} />
+        </div>
+      )}
+      <span className="placeholder">{placeholderValue}</span>
+      {checked ? (
+        <>
+          <label className="option">
+            <input
+              type="radio"
+              name="option"
+              value="Mercedes"
+              onChange={handleInputClickedValue}
+              onClick={handleDropdownMenu}
+            />
+            <span className="title">Mercedes</span>
+          </label>
+          <label className="option">
+            <input
+              type="radio"
+              name="option"
+              value="Opel"
+              onChange={handleInputClickedValue}
+              onClick={handleDropdownMenu}
+            />
+            <span className="title">Opel</span>
+          </label>
+          <label className="option">
+            <input
+              type="radio"
+              name="option"
+              value="Audi"
+              onChange={handleInputClickedValue}
+              onClick={handleDropdownMenu}
+            />
+            <span className="title">Audi</span>
+          </label>
+        </>
+      ) : null}
+    </div>
   );
 }
 
