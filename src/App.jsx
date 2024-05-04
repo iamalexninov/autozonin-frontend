471153497cf892f26767aca3da4cbd4dc85cbde8
Alex Georgiev Ninov <iamalexgninov@gmail.com>
2024-05-04 19:29:13 +0300

    up images

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import "./assets/styles/index.css";
-import { GlobalUnderline } from "./components/global/underline/GlobalUnderline";
+import suv from "./assets/svg/hatchback.svg";
+
 
 function App() {
-  return <GlobalUnderline />;
+  const styles = {
+    backgroundColor:'var(--orange)',
+    padding:'10px 20px'
+  }
+
+
+  return (
+    <>
+      <img style={styles} src={suv} alt="" width={60} height={30} />
+    </>
+  );
 }
 
 export default App;
