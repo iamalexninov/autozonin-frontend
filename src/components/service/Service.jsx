6a9bace5d4a49799d4516d2a5a01e3f6bc18c8ae
Alex Georgiev Ninov <iamalexgninov@gmail.com>
2024-05-11 15:09:26 +0300

    added: service features section

--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -1,14 +1,13 @@
 import styles from "./style.module.css";
 
-import { Wrapper } from "../global/wrapper/Wrapper";
 import { ServiceWelcome } from "./service-welcome/ServiceWelcome";
+import { ServiceFeatures } from "./service-features/ServiceFeatures";
 
 export const Service = () => {
   return (
     <section className={styles.service}>
-      <Wrapper>
-        <ServiceWelcome />
-      </Wrapper>
+      <ServiceWelcome />
+      <ServiceFeatures />
     </section>
   );
 };
--- src/components/service/service-features/ServiceFeatures.jsx
+++ src/components/service/service-features/ServiceFeatures.jsx
@@ -1,5 +1,41 @@
 import styles from "./style.module.css";
 
+import { Wrapper } from "../../global/wrapper/Wrapper";
+import { ServiceFeaturesList } from "./ServiceFeaturesList";
+
 export const ServiceFeatures = () => {
-  return <div>ServiceFeatures</div>;
+  const mockdata = [
+    {
+      id: "01.",
+      heading: "virtual showroom tours",
+      description:
+        "Explore our diverse inventory online with detailed photos and specifications, and even schedule virtual test drives for added convenience.",
+    },
+    {
+      id: "02.",
+      heading: "transparent pricing",
+      description:
+        "Enjoy clear and upfront pricing on all vehicles, ensuring a hassle-free shopping experience without any hidden fees.",
+    },
+    {
+      id: "03.",
+      heading: "personalized financing",
+      description:
+        "Our finance experts offer tailored solutions to fit your needs and budget, including competitive rates and flexible repayment plans.",
+    },
+    {
+      id: "04.",
+      heading: "expectional service",
+      description:
+        "Experience friendly and attentive customer service from our knowledgeable team, dedicated to making your car buying journey seamless and satisfying.",
+    },
+  ];
+
+  return (
+    <div className={styles.service_features}>
+      <Wrapper>
+        <ServiceFeaturesList features={mockdata} />
+      </Wrapper>
+    </div>
+  );
 };
