e9d0e99a43e47d7aab6df92b878efd65f6e1dd80
Alex Georgiev Ninov <iamalexgninov@gmail.com>
2024-05-14 23:17:46 +0300

    added: toggle faq question

--- src/components/faq/faq-items/FAQQuestion.jsx
+++ src/components/faq/faq-items/FAQQuestion.jsx
@@ -1,15 +1,25 @@
-import { GlobalIcon } from "../../global/GlobalIcon";
 import styles from "./style.module.css";
 
+import { useState } from "react";
+
+import { GlobalIcon } from "../../global/GlobalIcon";
+
 export const FAQQuestion = ({ question, answer }) => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    setToggle((prev) => !prev);
+  };
+
   return (
     <li className={styles.question_container}>
       <div className={styles.question}>
         <h5 className={styles.question_title}>{question}</h5>
-        <div className={styles.question_icon}>
-          <GlobalIcon type="plus" />
+        <div className={styles.question_icon} onClick={handleToggle}>
+          <GlobalIcon type={toggle ? "minus" : "plus"} />
         </div>
       </div>
+      {toggle && <p className={styles.answer}>{answer}</p>}
     </li>
   );
 };
