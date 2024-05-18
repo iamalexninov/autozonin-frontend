6b4add3e71e60334a9a82907a942c31b98e153fe
Alex Georgiev Ninov <iamalexgninov@gmail.com>
2024-05-18 07:09:21 +0300

    fix: deleted components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,9 @@
 import { Hero } from "../components/hero/Hero";
-import { Service } from "../components/service/Service";
-import { BrowseSection } from "../components/browse/Browse";
-import { FeaturedVehicles } from "../components/featured-vehicles/FeaturedVehicles";
-import { AutoHub } from "../components/auto-hub/AutoHub";
-import { AutoMagazine } from "../components/auto-magazine/AutoMagazine";
 
 export const Home = () => {
   return (
     <>
       <Hero />
-      <Service />
-      <BrowseSection />
-      <FeaturedVehicles />
-      <AutoHub />
-      <AutoMagazine />
     </>
   );
 };
