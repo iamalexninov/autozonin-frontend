3d9fffc1f5e9a8bb88703f1c0ee6ccd4e72251f9
Alex Georgiev Ninov <iamalexgninov@gmail.com>
2024-05-21 22:10:02 +0300

    added: autohub

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import { Stats } from "../components/stats/Stats";
 import { WhyChooseUs } from "../components/why-choose-us/WhyChooseUs";
 import { PopularMakes } from "../components/popular-makes/PopularMakes";
 import { LatestBlogPosts } from "../components/latest-blog-posts/LatestBlogPosts";
+import { AutoHub } from "../components/autohub/AutoHub";
 
 export const Home = () => {
   return (
@@ -18,6 +19,7 @@ export const Home = () => {
       <WhyChooseUs />
       <PopularMakes />
       <LatestBlogPosts />
+      <AutoHub />
     </>
   );
 };
