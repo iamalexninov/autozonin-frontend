279d057167f0e1a45c27189b94ed8226144b4590
Alex Georgiev Ninov <iamalexgninov@gmail.com>
2024-05-22 20:43:10 +0300

    added: footer bottom

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,14 @@
 import styles from "./style.module.css";
 import { Wrapper } from "../global/wrapper/Wrapper";
 import { FooterTop } from "./footer-top/FooterTop";
+import { FooterBottom } from "./footer-bottom/FooterBottom";
 
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
       <Wrapper>
         <FooterTop />
+        <FooterBottom />
       </Wrapper>
     </footer>
   );
