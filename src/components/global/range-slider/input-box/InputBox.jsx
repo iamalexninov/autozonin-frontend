b3b19f6a2d5ba44f6576b72a428009b1bbfe4375
Alex Georgiev Ninov <iamalexgninov@gmail.com>
2024-05-26 15:18:58 +0300

    fix: range slider - separate components

--- src/components/global/range-slider/input-box/InputBox.jsx
+++ src/components/global/range-slider/input-box/InputBox.jsx
@@ -1,7 +1,24 @@
-import styles from './style.module.css';
+import styles from "./style.module.css";
+import { TextField } from "../../text-field/TextField";
 
 export const InputBox = () => {
   return (
-    <div>InputBox</div>
-  )
-}
+    <div className={styles.input_box}>
+      <div className={styles.min_box}>
+        <RenderField placeholder="Min.." name="min_value" />
+      </div>
+      <div className={styles.max_box}>
+        <RenderField placeholder="Max.." name="max_value" />
+      </div>
+    </div>
+  );
+};
+
+const RenderField = ({ placeholder, name }) => {
+  return (
+    <div className={styles.input_wrap}>
+      <span className={styles.input_addon}>EU</span>
+      <TextField type="input" placeholder={placeholder} name={name} />
+    </div>
+  );
+};
