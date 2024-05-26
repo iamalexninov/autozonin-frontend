08146bc0d963cf4113ebeca306ed38d0a0af0852
Alex Georgiev Ninov <iamalexgninov@gmail.com>
2024-05-26 13:45:17 +0300

    added: offices component

--- src/components/contact-us/ContactUs.jsx
+++ src/components/contact-us/ContactUs.jsx
@@ -3,6 +3,7 @@ import styles from "./style.module.css";
 import { Wrapper } from "../global/wrapper/Wrapper";
 import { GetInTouch } from "./get-in-touch/GetInTouch";
 import { ContactDetails } from "./contact-details/ContactDetails";
+import { Offices } from "./offices/Offices";
 
 export const ContactUs = () => {
   return (
@@ -12,6 +13,7 @@ export const ContactUs = () => {
           <GetInTouch />
           <ContactDetails />
         </div>
+        <Offices />
       </Wrapper>
     </section>
   );
