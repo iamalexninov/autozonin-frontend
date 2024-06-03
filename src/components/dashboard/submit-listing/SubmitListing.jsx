b806b84332f4d166affad008b197b169d60f337f
Alex Georgiev Ninov <iamalexgninov@gmail.com>
2024-06-03 23:17:40 +0300

    fix: responsive submit-listing categories

--- src/components/dashboard/submit-listing/SubmitListing.jsx
+++ src/components/dashboard/submit-listing/SubmitListing.jsx
@@ -4,18 +4,11 @@ import { Form } from "../../global/form/Form";
 import { Categories } from "./categories/Categories";
 
 export const SubmitListing = () => {
-  // [] - main details form
-  // [] - dimensions & capacity
-  // [] - engine & transmission
-  // [] - media form - upload images
-  // [] - location
-
   return (
     <div className={["dashboard_section", styles.submit].join(" ")}>
       <h3 className={styles.submit_title}>Add Vehicle Listing</h3>
       <Form>
-        <div>submit</div>
-        {/* <Categories /> */}
+        <Categories />
       </Form>
     </div>
   );
