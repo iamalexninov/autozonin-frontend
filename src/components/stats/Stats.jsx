2bbd7b75fe966d4a207b3b360f38567f737a1c52
Alex Georgiev Ninov <iamalexgninov@gmail.com>
2024-06-09 12:27:07 +0300

    fix: stylres on stats and why choose us

--- src/components/stats/Stats.jsx
+++ src/components/stats/Stats.jsx
@@ -1,31 +1,13 @@
 import styles from "./style.module.css";
+import { stats } from "./Text";
 
 import { Wrapper } from "../global/wrapper/Wrapper";
 
 export const Stats = () => {
-  const statsmockdata = [
-    {
-      type: "cars for sale",
-      value: "30K+",
-    },
-    {
-      type: "dealer reviews",
-      value: "2K+",
-    },
-    {
-      type: "visitors per day",
-      value: "1K+",
-    },
-    {
-      type: "verified dealers",
-      value: "1.5K+",
-    },
-  ];
-
   return (
     <section className={styles.stats}>
       <Wrapper>
-        <StatsRecords records={statsmockdata} />
+        <StatsRecords records={stats} />
       </Wrapper>
     </section>
   );
@@ -41,11 +23,11 @@ const StatsRecords = ({ records }) => {
   );
 };
 
-const StatsRecord = ({record}) => {
+const StatsRecord = ({ record }) => {
   return (
     <li className={styles.stats_record}>
-      <p className={styles.stats_record_value}>{record.value}</p>
-      <p className={styles.stats_record_type}>{record.type}</p>
+      <p className={styles.stats_value}>{record.value}</p>
+      <p className={styles.stats_type}>{record.type}</p>
     </li>
   );
 };
--- src/components/why-choose-us/WhyChooseUs.jsx
+++ src/components/why-choose-us/WhyChooseUs.jsx
@@ -1,32 +1,14 @@
 import styles from "./style.module.css";
+import { features } from "./Text";
 
 import { Wrapper } from "../global/wrapper/Wrapper";
 
 export const WhyChooseUs = () => {
-  const featuresmockdata = [
-    {
-      type: "Comprehensive Inventory Management System",
-      desc: "An integrated system for managing and displaying new and used car inventories online, allowing customers to easily browse and check real-time availability.",
-    },
-    {
-      type: "Customer Relationship Management System",
-      desc: "A CRM tailored for dealerships helps manage customer interactions, track leads, schedule test drives, and improve follow-ups, fostering long-term relationships and increasing sales.",
-    },
-    {
-      type: "On-Site Financing and Insurance Services",
-      desc: "Providing financing and insurance options on-site streamlines the purchasing process, enabling customers to complete all necessary paperwork conveniently at the dealership.",
-    },
-    {
-      type: "Service and Maintenance Department",
-      desc: "A fully equipped service department offering routine maintenance, repairs, and parts replacement enhances post-sale customer satisfaction and encourages repeat business.",
-    },
-  ];
-
   return (
     <section className={styles.features}>
       <Wrapper>
-        <h3 className={styles.features_header}>Why Choose Us?</h3>
-        <FeaturesRecords records={featuresmockdata} />
+        <h3 className={styles.features_title}>Why Choose Us?</h3>
+        <FeaturesRecords records={features} />
       </Wrapper>
     </section>
   );
@@ -45,8 +27,8 @@ const FeaturesRecords = ({ records }) => {
 const FeaturesRecord = ({ record }) => {
   return (
     <li className={styles.features_record}>
-      <p className={styles.features_record_type}>{record.type}</p>
-      <p className={styles.features_record_desc}>{record.desc}</p>
+      <p className={styles.features_type}>{record.type}</p>
+      <p className={styles.features_desc}>{record.desc}</p>
     </li>
   );
 };
